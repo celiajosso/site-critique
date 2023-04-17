734902fa74200be39b10ccde06208a8271be70d5
CelJos <celia.josso@orange.fr>
2023-04-17 16:57:14 +0200

    ajout des nouvels utilisateur quand inscription

--- php/inscription.php
+++ php/inscription.php
@@ -10,7 +10,7 @@ require_once("../includes/constantes.php");      //constantes du site
 require_once("../includes/config-bdd.php");      //constantes du site
 include_once("../php/functions-DB.php");
 include_once("../php/functions_query.php");
-include_once("../php/functions_structure.php");
+//include_once("../php/functions_structure.php");
 $my_sqli = connectionDB();
 
 ?>
@@ -18,25 +18,15 @@ $my_sqli = connectionDB();
 <?php
 
 $nom = $_POST["nom"];
-
 $prenom = $_POST["prenom"];
-
 $naissance = $_POST["naissance"];
-
 $mail = $_POST["mail"];
-
 $login = $_POST["login"];
-
 $mdp = $_POST["mdp"];
-
 $mdp_confirmation = $_POST["mdp-confirmation"];
-
 $choix_pp = $_POST["choix-pp"];
-
 $login_unique = Is_loginUnique($my_sqli, $login);
-
 $age_valide = Is_enoughAged($naissance);
-
 $mdp_valide = Is_samePassword($mdp, $mdp_confirmation);
 
 if (!$age_valide) {
@@ -49,6 +39,10 @@ elseif (!$mdp_valide) {
     header("Location: ../registration.php?nom=$nom&prenom=$prenom&mail=$mail&naissance=$naissance&login=$login&mdp=$mdp&mdp_confirmation=$mdp_confirmation&choix_pp=$choix_pp&erreur=mdp");
 }
 else{
+    $img_path = "Images/PhotoProfil/" . $choix_pp;
+    $today = date("Y-m-d");
+    $sql_input = "INSERT INTO Utilisateur (login_Utilisateur, password_Utilisateur, photoProfil_Utilisateur, nom_Utilisateur, prenom_Utilisateur, id_role, mail_Utilisateur, dateNaissance_Utilisateur, dateCreation_Utilisateur, dateConnexion_Utilisateur) VALUES ('$login', '$mdp', '$img_path', '$nom', '$prenom', 1, '$mail', '$naissance', '$today', NULL)";
+    $sql_input_res = writeDB($my_sqli, $sql_input);
     header("Location: ../index.php");
 }
 ?>
