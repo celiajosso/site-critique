546d68233cb5daf1334b8f9d2859c1b41e19f892
CelJos <celia.josso@orange.fr>
2023-04-27 23:22:10 +0200

    ajout du prix du jeu Rocket League

--- site-critique.sql
+++ site-critique.sql
@@ -37,7 +37,7 @@ INSERT INTO Jeu (nom, prix, date_sortie, synopsis) VALUES
 ("Minecraft","20.00","2011-11-18","Minecraft plonge le joueur dans un monde créé de manière procédurale, composé de voxels (des cubes) représentant différents matériaux comme de la terre, du sable, de la pierre, de l'eau, de la lave ou des minerais (du fer, de l'or, du charbon, etc. ) formant diverses structures (arbres, cavernes, montagnes, temples)."),
 ("Grand Theft Auto 5","19.99","2013-09-17","L'histoire solo suit trois protagonistes : le braqueur de banque à la retraite Michael De Santa, le gangster Franklin Clinton et le trafiquant de drogue et d'armes Trevor Philips, et leurs braquages sous la pression d'une agence gouvernementale corrompue et de puissants criminels."),
 ("Just Dance 22","26.54","2021-11-04","Just Dance 2023 Edition est un jeu de rythme et de danse développé par Ubisoft Paris et édité par Ubisoft."),
-("Rocket League","00.00","2015-07-07","Deux équipes, composées de un à quatre joueurs conduisant des véhicules, s'affrontent au cours d'un match afin de frapper un ballon et de marquer dans le but adverse. Les voitures sont équipées de propulseurs (boost) et peuvent sauter, permettant de jouer le ballon dans les airs.");
+("Rocket League","31.99","2015-07-07","Deux équipes, composées de un à quatre joueurs conduisant des véhicules, s'affrontent au cours d'un match afin de frapper un ballon et de marquer dans le but adverse. Les voitures sont équipées de propulseurs (boost) et peuvent sauter, permettant de jouer le ballon dans les airs.");
 
 /* table Role */
 
