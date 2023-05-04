212b57aa78882248355275e90a080acc5f4ddcf7
CelJos <celia.josso@orange.fr>
2023-05-04 20:21:56 +0200

    Update of some database values

--- site-critique.sql
+++ site-critique.sql
@@ -102,12 +102,12 @@ CREATE TABLE Utilisateur (
 );
 
 INSERT INTO Utilisateur (login_Utilisateur, password_Utilisateur, photoProfil_Utilisateur, nom_Utilisateur, prenom_Utilisateur, id_role, mail_Utilisateur, dateNaissance_Utilisateur, dateCreation_Utilisateur, dateConnexion_Utilisateur) VALUES
-    ("prannou", "prof_de_SR", "Images/PhotoProfil/1.png", "RANNOU", "Phillipe", 2, "prannou@gmail.com", "1988-02-16", "2022-04-01", "2023-04-15 16:00:02"),
-    ("rmounier", "prof_de_BDD", "Images/PhotoProfil/2.png", "MOUNIER", "Romain", 3, "rmounier@gmail.com", "1991-07-03", "2021-09-07", "2023-04-02 15:23:59"),
-    ("hfeuillatre", "prof_de_IHM", "Images/PhotoProfil/3.png", "FEUILLATRE", "Hélène", 3, "hfeuillatre@gmail.com", "1991-05-28", "2021-01-01", "2023-04-17 09:12:32"),
-    ("fthibault", "prof_de_physique", "Images/PhotoProfil/4.png", "THIBAULT", "Franck", 2, "fthibault@gmail.com", "1965-05-28", "2022-08-08", "2023-01-06 20:06:48"),
-    ("olafond", "prof_delectronique", "Images/PhotoProfil/5.png", "LAFOND", "Olivier", 1, "olafond@gmail.com", "1973-01-19", "2023-03-03", "2023-04-10 14:03:15"),
-    ("vbouquet", "prof_de_chimie", "Images/PhotoProfil/6.png", "BOUQUET", "Valérie", 1, "vbouquet@gmail.com", "1973-10-12", "2023-02-16", "2023-04-01 19:02:47");
+    ("prannou", "prof_de_SR", "Images/PhotoProfil/1.png", "RANNOU", "Phillipe", 2, "prannou@gmail.com", "1988-02-16", "2022-04-01", "2023-05-05 10:02:02"),
+    ("rmounier", "prof_de_BDD", "Images/PhotoProfil/2.png", "MOUNIER", "Romain", 3, "rmounier@gmail.com", "1991-07-03", "2021-09-07", "2023-05-05 09:30:59"),
+    ("hfeuillatre", "prof_de_IHM", "Images/PhotoProfil/3.png", "FEUILLATRE", "Hélène", 3, "hfeuillatre@gmail.com", "1991-05-28", "2021-01-01", "2023-05-05 07:12:32"),
+    ("fthibault", "prof_de_physique", "Images/PhotoProfil/4.png", "THIBAULT", "Franck", 2, "fthibault@gmail.com", "1965-05-28", "2022-08-08", "2023-05-04 22:53:48"),
+    ("olafond", "prof_delectronique", "Images/PhotoProfil/5.png", "LAFOND", "Olivier", 1, "olafond@gmail.com", "1973-01-19", "2023-03-03", "2023-05-01 13:07:15"),
+    ("vbouquet", "prof_de_chimie", "Images/PhotoProfil/6.png", "BOUQUET", "Valérie", 1, "vbouquet@gmail.com", "1973-10-12", "2023-02-16", "2023-04-29 08:36:47");
 
 /* table Article */
 
